cd1b0d4dfdba141f6779be8b02d465c22d39d327
David Park <davepark97@berkeley.edu>
2022-10-05 12:13:25 -0700

    Add an event listener for the add button
    When an user clicks on the add button, the modal
    box's background's display will change from none
    to block. To put it simply, upon the user clicking
    the add button, the modal box's background will now
    be visible to the user.

---
+++ script.js
@@ -0,0 +1,6 @@
+const ADD_BUTTON = document.querySelector('.add');
+const MODAL_BACKGROUND = document.querySelector('.modal-background');
+
+ADD_BUTTON.addEventListener("click", () => {
+  MODAL_BACKGROUND.style.display = "block";
+}); 
