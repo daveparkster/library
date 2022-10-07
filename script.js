3df19ed67cfdf44a0cd79c4937703f97065f0364
David Park <davepark97@berkeley.edu>
2022-10-06 21:13:22 -0700

    Add comment for further clarification

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const ADD_BUTTON = document.querySelector('.add');
 const MODAL_BACKGROUND = document.querySelector('.modal-background');
 
+/*upon clicking the addbutton, the modal appears on the screen*/
 ADD_BUTTON.addEventListener("click", () => {
   MODAL_BACKGROUND.style.display = "block";
 }); 
+
