dd66313ccda5a3e47d3e8019d80e87f56889693b
David Park <davepark97@berkeley.edu>
2022-10-08 19:18:54 -0700

    Add event listener in order to exit form
    The event listener was attached to the window
    in order for the user to exit from form.

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const MODAL_BACKGROUND = document.querySelector('.modal-background');
 
 /*upon clicking the addbutton, the modal appears on the screen*/
 ADD_BUTTON.addEventListener("click", () => {
-  MODAL_BACKGROUND.style.display = "block";
+  MODAL_BACKGROUND.style.display = "flex";
 }); 
+
+/*when the user clicks outside of form, the form 
+  disappears*/
+window.addEventListener("click", (event) => {
+  if(event.target == MODAL_BACKGROUND) {
+    MODAL_BACKGROUND.style.display = "none";
+  }
+});
 
