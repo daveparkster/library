823b265a4828bb2b94d748e2aebbf80ab2eaa574
David Park <davepark97@berkeley.edu>
2022-10-09 21:11:03 -0700

    Define a new object constructor
    Using the four values, the Book constructor
    takes four arguments and assigns the value of
    each arguments to its individual keys. Then,
    the new Object is then pushed to the array
    referenced by the variable myLibrary where
    it will store all books.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const ADD_BUTTON = document.querySelector('.add');
 const MODAL_BACKGROUND = document.querySelector('.modal-background');
+const FORM = document.querySelector('form');
+
 
 /*upon clicking the addbutton, the modal appears on the screen*/
 ADD_BUTTON.addEventListener("click", () => {
@@ -14,17 +16,28 @@ window.addEventListener("click", (event) => {
   }
 });
 
-/*attach event listener to form submission*/
-const FORM = document.querySelector('form');
+/*where user input will be stored*/
+let myLibrary = [];
 
 FORM.addEventListener('submit', getForm);
 
-function getForm(event) {
-  event.preventDefault();
+function getForm(e) {
+  e.preventDefault();
 
   let title = document.querySelector('#title').value;
   let author = document.querySelector('#author').value;
   let pages = document.querySelector('#pages').value;
   let read = document.querySelector('#read').value;
+
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+/*Object constructor*/
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
